619ad201190d36a6ff9dc5de719a0d7fa9dcfeb8
ivzaislu <johann0789@googlemail.com>
2025-03-05 02:33:25 +0100

    Create pjatiletka.js

---
+++ pjatiletka.js
@@ -0,0 +1,90 @@
+"use strict";
+
+(function () {
+    var addPyatiletkaToMenu = function () {
+        var menuItem = $("<li class='menu__item selector' data-action='mad_pyatiletka'>" +
+            "<div class='menu__ico'>" +
+            "<svg width='24' height='24' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>" +
+            "<circle cx='12' cy='12' r='10' stroke='currentColor' stroke-width='2'></circle>" +
+            "<text x='6' y='16' fill='currentColor' font-size='12' font-family='Arial' font-weight='bold'>5</text>" +
+            "</svg>" +
+            "</div>" +
+            "<div class='menu__text'>Пятилетка</div>" +
+            "</li>");
+
+        Lampa.Menu.render().find("[data-action='catalog']").before(menuItem);
+
+        menuItem.on("hover:enter", function () {
+            loadMoviesByPeriod();
+        });
+    };
+
+    var loadMoviesByPeriod = function () {
+        Lampa.Api.request({
+            method: 'discover',
+            source: 'tmdb',
+            param: {
+                sort_by: 'popularity.desc',
+                primary_release_date_gte: '2000-01-01', // Отфильтруем фильмы, начиная с 2000 года
+                primary_release_date_lte: '2024-12-31',
+                with_original_language: 'ru,en'
+            }
+        }, function (data) {
+            if (data.results && data.results.length) {
+                var groupedMovies = groupMoviesByPeriod(data.results);
+                openPyatiletkaScreen(groupedMovies);
+            } else {
+                Lampa.Noty.show("Фильмы не найдены");
+            }
+        }, function (error) {
+            Lampa.Noty.show("Ошибка загрузки данных");
+        });
+    };
+
+    var groupMoviesByPeriod = function (movies) {
+        var periods = {
+            "2020-2024": [],
+            "2015-2019": [],
+            "2010-2014": [],
+            "2005-2009": [],
+            "2000-2004": []
+        };
+
+        movies.forEach(function (movie) {
+            var year = parseInt(movie.release_date ? movie.release_date.substring(0, 4) : "2000");
+
+            if (year >= 2020) periods["2020-2024"].push(movie);
+            else if (year >= 2015) periods["2015-2019"].push(movie);
+            else if (year >= 2010) periods["2010-2014"].push(movie);
+            else if (year >= 2005) periods["2005-2009"].push(movie);
+            else periods["2000-2004"].push(movie);
+        });
+
+        return periods;
+    };
+
+    var openPyatiletkaScreen = function (groupedMovies) {
+        var items = [];
+
+        Object.keys(groupedMovies).forEach(function (period) {
+            if (groupedMovies[period].length) {
+                items.push({
+                    title: period,
+                    results: groupedMovies[period]
+                });
+            }
+        });
+
+        Lampa.Activity.push({
+            url: "",
+            title: "Фильмы по пятилеткам",
+            component: "category",
+            page: 1,
+            items: items
+        });
+    };
+
+    window.appready ? addPyatiletkaToMenu() : Lampa.Listener.follow("app", function (event) {
+        if (event.type === "ready") addPyatiletkaToMenu();
+    });
+})();
