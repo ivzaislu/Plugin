e05426f1b277304a9bb06a26538aece213084026
ivzaislu <johann0789@googlemail.com>
2025-03-05 09:25:44 +0100

    Update ruka.js

--- Lampa/ruka.js
+++ Lampa/ruka.js
@@ -1,4 +1,4 @@
-
+"use strict";
 
 (function() {
   var apiKey = '9cdde3c5';  // Ваш API ключ для OMDb
@@ -9,23 +9,27 @@
     var url = `http://www.omdbapi.com/?apikey=${apiKey}&type=movie&plot=short&r=json`;
 
     fetch(url)
-      .then(response => response.json())
-      .then(data => {
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
         if (data.Response === "True") {
           // Фильтруем фильмы по жанру
-          var filteredMovies = data.Search.filter(movie => movie.Genre && movie.Genre.includes(genre));
+          var filteredMovies = data.Search.filter(function(movie) {
+            return movie.Genre && movie.Genre.includes(genre);
+          });
           
           if (filteredMovies.length > 0) {
             // Отображаем отфильтрованные фильмы
             displayMovies(filteredMovies);
           } else {
-            console.log(`Нет фильмов жанра "${genre}".`);
+            console.log("Нет фильмов жанра \"" + genre + "\".");
           }
         } else {
           console.log("Ошибка при получении данных:", data.Error);
         }
       })
-      .catch(error => {
+      .catch(function(error) {
         console.error("Ошибка при выполнении запроса:", error);
       });
   };
@@ -36,11 +40,11 @@
 
     movies.forEach(function(movie) {
       var movieItem = $(
-        `<div class="movie-item">
-           <h3>${movie.Title} (${movie.Year})</h3>
-           <p>${movie.Genre}</p>
-           <img src="${movie.Poster}" alt="${movie.Title}" />
-         </div>`
+        '<div class="movie-item">' +
+          '<h3>' + movie.Title + ' (' + movie.Year + ')</h3>' +
+          '<p>' + movie.Genre + '</p>' +
+          '<img src="' + movie.Poster + '" alt="' + movie.Title + '" />' +
+        '</div>'
       );
       movieList.append(movieItem);
     });
@@ -51,7 +55,14 @@
 
   // Функция для добавления кнопки в меню Lampa
   var addButtonToMenu = function() {
-    var button = $('<li class="menu__item selector" data-action="show_movies_genre"><div class="menu__ico"><svg width="24" height="23" viewBox="0 0 24 23" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM12 20C7.03 20 4 16.97 4 12C4 7.03 7.03 4 12 4C16.97 4 20 7.03 20 12C20 16.97 16.97 20 12 20Z" stroke="currentColor" stroke-width="2.5"></path></svg></div><div class="menu__text">Фильмы по жанрам</div></li>');
+    var button = $('<li class="menu__item selector" data-action="show_movies_genre">' +
+      '<div class="menu__ico">' +
+        '<svg width="24" height="23" viewBox="0 0 24 23" fill="none" xmlns="http://www.w3.org/2000/svg">' +
+          '<path d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM12 20C7.03 20 4 16.97 4 12C4 7.03 7.03 4 12 4C16.97 4 20 7.03 20 12C20 16.97 16.97 20 12 20Z" stroke="currentColor" stroke-width="2.5"></path>' +
+        '</svg>' +
+      '</div>' +
+      '<div class="menu__text">Фильмы по жанрам</div>' +
+    '</li>');
 
     // Добавляем обработчик события для кнопки
     button.on("hover:enter", function() {
@@ -63,7 +74,13 @@
   };
 
   // Запускаем добавление кнопки и выполнение запроса после загрузки приложения
-  window.appready ? addButtonToMenu() : Lampa.Listener.follow("app", function(b) {
-    if (b.type === "ready") addButtonToMenu();
-  });
+  if (window.appready) {
+    addButtonToMenu();
+  } else {
+    Lampa.Listener.follow("app", function(b) {
+      if (b.type === "ready") {
+        addButtonToMenu();
+      }
+    });
+  }
 })();
