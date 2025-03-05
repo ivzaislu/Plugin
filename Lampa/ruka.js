4d5718e559dbd41160c4a6e37e0b6bc570fa9005
ivzaislu <johann0789@googlemail.com>
2025-03-05 09:20:26 +0100

    Update ruka.js

--- Lampa/ruka.js
+++ Lampa/ruka.js
@@ -49,8 +49,21 @@
     Lampa.Menu.render().find('[data-component="menu"]').append(movieList);
   };
 
-  // Запускаем запрос
-  window.appready ? fetchMoviesByGenre() : Lampa.Listener.follow("app", function(b) {
-    if (b.type === "ready") fetchMoviesByGenre();
+  // Функция для добавления кнопки в меню Lampa
+  var addButtonToMenu = function() {
+    var button = $('<li class="menu__item selector" data-action="show_movies_genre"><div class="menu__ico"><svg width="24" height="23" viewBox="0 0 24 23" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM12 20C7.03 20 4 16.97 4 12C4 7.03 7.03 4 12 4C16.97 4 20 7.03 20 12C20 16.97 16.97 20 12 20Z" stroke="currentColor" stroke-width="2.5"></path></svg></div><div class="menu__text">Фильмы по жанрам</div></li>');
+
+    // Добавляем обработчик события для кнопки
+    button.on("hover:enter", function() {
+      fetchMoviesByGenre();  // При нажатии на кнопку, выполняем запрос
+    });
+
+    // Добавляем кнопку в меню Lampa
+    Lampa.Menu.render().find('[data-component="menu"]').append(button);
+  };
+
+  // Запускаем добавление кнопки и выполнение запроса после загрузки приложения
+  window.appready ? addButtonToMenu() : Lampa.Listener.follow("app", function(b) {
+    if (b.type === "ready") addButtonToMenu();
   });
 })();
